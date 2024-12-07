0c7bdda8b593106cf60344aaea7008cedcce8d9b
0xb0 <88857717+0xb0-Celestial@users.noreply.github.com>
2024-12-07 16:48:22 +0000

    Update STEP 3. macro.py

--- STEP 3. macro.py
+++ STEP 3. macro.py
@@ -1,25 +1,55 @@
-from keyboard import press_and_release
+from keyboard import press,release
+from time import sleep
 from pyautogui import pixel
 from win32gui import GetForegroundWindow, SetWindowPos
 from win32con import HWND_TOPMOST
+from multiprocessing import Process
 
-y = 200
-hwnd = GetForegroundWindow()
-SetWindowPos(hwnd,HWND_TOPMOST,100,100,475,300,0)
+y = 190
 
-print("important! add these arrow binds: \nLeft-F\nUp-G\nDown-K\nRight-L")
-print("btw it can't hold hold notes \n so play maps with the least hold notes")
-print("observing pixels...")
-while True:
-    if pixel(730,y)[0] == 194:
-        press_and_release("f")
-        print("pressed LeftArrow")
-    if pixel(880,y)[0] == 0:
-        press_and_release("g")
-        print("pressed UpArrow")
-    if pixel(1030,y)[0] == 18:
-        press_and_release("k")
-        print("pressed DownArrow")
-    if pixel(1200,y)[0] == 249:
-        press_and_release("l")
-        print("pressed RightArrow")
+def LeftArrow():
+    while True:
+        if pixel(735,y)[1] == 75:
+            press("f")
+            if pixel(735,y+51)[1] != 75:
+                release("f")
+def DownArrow():
+    while True:
+        if pixel(885,y)[1] == 255:
+            press("g")
+            if pixel(885,y+51)[1] != 255:
+                release("g")
+def UpArrow():
+    while True:
+        if pixel(1034,y)[1] == 250:
+            press("k")
+            if pixel(1034,y+51)[1] != 250:
+                release("k")
+def RightArrow():
+    while True:
+        if pixel(1180,y)[1] == 57:
+            press("l")
+            if pixel(1180,y+51)[1] != 57:
+                release("l")
+
+if __name__ == "__main__":
+    hwnd = GetForegroundWindow()
+    SetWindowPos(hwnd,HWND_TOPMOST,100,100,475,300,0)
+    print("important! add these arrow binds: \nLeft-F\nUp-G\nDown-K\nRight-L")
+    print("observing pixels...")
+    p1 = Process(target=LeftArrow)
+    p1.start()
+    print("task 1 ready!")
+    p2 = Process(target=UpArrow)
+    p2.start()
+    print("task 2 ready!")
+    p3 = Process(target=DownArrow)
+    p3.start()
+    print("task 3 ready!")
+    p4 = Process(target=RightArrow)
+    p4.start()
+    print("task 4 ready!")
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join() 
